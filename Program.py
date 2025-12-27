9239913a138c2f9458463899093371efa5be917e
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2025-12-27 08:43:41 +0530

    Add files

---
+++ Program.py
@@ -0,0 +1,613 @@
+# # print("Hello world","Rushikesh Kasav",22+90) 
+
+
+# name = "Rushikesh"
+# age = 22
+# price = 25.99
+# age2 = age
+# # print(price+2)
+# # print("My Age is: ",age2)
+# # print("And my name is: ",name)
+# # print(type(name))
+# # print(type(age))
+# # print(type(price))
+# name1 = 'rk'
+# name2 = "rk"
+# name3 = '''rk'''
+# print(name1,name2,name3)
+
+# age = 23
+# old = False
+# a = None
+
+# print(type(old))
+# print(type(age))
+# print(type(a))
+# a =2
+# b =2
+# sum = a+b
+# print(sum)
+
+# Operators
+# Arithmetic operators = (+,-,*,/,%,**)
+# relational            =(==,!=,>,<,<=,>=)
+# Assignment        =(-, +=,-=,*=,/=%=,**=)
+# Logical          =(and,not,or)
+
+# AO
+# a=5
+# b=2
+
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a+b)
+
+# print(a%b)
+# print(a**b)
+# print(a==b)
+# print(a != b)
+# print(a >= b)
+# print(a<=b)
+
+
+# num = 10
+# num /= 5
+# print("num: ",num) #2
+# a = 50
+# b = 30
+# print(not False)
+# print(not a>b)
+
+# val1 = True
+# val2 = True
+# print("and operator:", val1 and val2)
+# print("Or operator: ", val1 or val2)
+
+#Type Conversion(automatic conversion)
+# a =int("2")
+# b=4.25
+# sum =a+b
+# print(type(a))
+# print(sum)
+
+# val = int(input("Enter some value: "))
+# print(type(val),val)
+
+# name = input("Enter name: ")
+# age = input("Enter age: ")
+# marks = input("Enter marks: ")
+
+# print("Welcome", name)
+# print("age = ",age)
+# print("marks =",marks)
+
+
+# Write a Program to input 2 numbers & print their sum
+# a = int((input("Enter value of the a: ")))
+# b = int((input("Enter value of the b: ")))
+
+# print(a+b)
+# Write a Program side of a square & print its area
+
+# side = int(input("Enter the size of side: "))
+# area = side*side
+# print(area)
+
+# Write a program to input 2 floating point numbers & print their average
+# num1 = float(input("Enter the value: "))
+# num2 = float(input("Enter the value of num2: "))
+
+# avg = (num1+num2)/2
+# print(avg)
+# print("avg = ",(num1+num2)/2)
+
+# a = int(input("Enter the a: "))
+# b = int(input("Enter the b: "))
+# print()
+
+# a,b = 1,"2.0"
+# print(sum = a+b)
+
+# a,b = 1,"2"
+# c = int(b)
+# sum = a+c
+# print(sum)
+
+# a = int(input("Enter the value a: "))
+# b = int(input("Enter the value b:"))
+# sum = a+b
+# print(sum)
+
+# side = int(input("Enter the size od side: "))
+# area = side*side
+# print(area)
+
+# num1 = float(input("Enter the num1: "))
+# num2 = float(input("Enter the num2: "))
+# avg = (num1+num2)/2
+# print(avg)
+
+# num1 = int(input("Enter the num1: "))
+# num2 = int(input("Enter the num2: "))
+
+# print(num1>num2)
+# str1 = "this is a Sting.\tHello guys Rushikesh here."
+# print(str1)
+# str1 = "Hello"
+# str2 = "College"
+# print(str1+str2)
+
+# Str1 = "helloworld"
+# print(len(Str1))
+
+# Str2 = "Rushikesh"
+# print(len(Str2))
+
+# final_str = Str1 + " " + Str2
+# print(final_str)
+
+# str = "Hello World"
+# ch = str[2]
+# print(ch)
+
+
+# str = "hello World"
+# slice = str[3:-1]
+# print(slice)
+
+# str = "Rushikesh kasav"
+# slice = str[-6:-1]
+# print(slice)
+
+# str = "Rushikesh kasav"
+# print(str.endswith("av"))
+
+
+# str = "rushikehs kasav"
+# print(str)
+# print(str.capitalize())
+
+# str = "Rushikesh kasav"
+# print(str.replace("Rushikesh","Harshad"))
+
+# str = "Rushikesh kasav"
+# print(str.find("kasav"))
+# str = "Rushikesh kasav"
+# print(str.count("sh"))
+
+# name = input("Enter the name: ")
+# name1 = len(name)
+# print(name1)
+
+# str = "rsuiysh$fdgjd$hdgkjbd$"
+# print(str.count("$"))
+
+# age =0
+# if(age >= 18):
+#     print("Can vote & apply for license")
+#     print("can drive & can vote")
+
+# elif(0 < age <18):
+#     print("bc bachha  hai tu")
+#     print("lund mazha")
+
+# else:
+#     print("sex karlo")
+
+# print("matherchod")
+
+# marks = 35
+# if(marks >= 90):
+#     print("Grade = A")
+# elif(90 >marks >= 80):
+#     print("Grade = B")
+# elif(80 > marks >=70):
+#     print("Garde = C")
+# else:
+#     print("Grade = D ( Fucker you are fail)")
+
+
+# age = 90
+
+# if(age >=18):
+#     if(age>=80):
+#         print("Cannot Drive")
+#     else:
+#         print("Can Drive")
+# else:
+#     print("Cnnot Drive")
+
+# num = int(input("Enter number: "))
+# if(num%2 == 0):
+#     print("Even")
+# else:
+#     print("ODD")
+
+# a = int(input("Enter the num1: "))
+# b = int(input("Enter the num2: "))
+# c = int(input("Enter the num3: "))
+
+# if(a>b and a>c):
+#     print(a)
+# elif(b>c and b>a):
+#     print(b)
+# elif(c>b and c>a):
+#     print(c)
+
+# a = int(input("Enter num1: "))
+# b = int(input("Enter num2: "))
+# c = int(input("Enter num3: "))
+# d = int(input("Enter num4: "))
+
+# if(a>b and a>c and a>d):
+#     print(a)
+# elif(b>a and b>c and b>d):
+#     print(b)
+# elif(c>a and c>b and c>d):
+#     print(c)
+# elif(d>a and d>b and d>c):
+#     print(d)
+# else:
+#     print("Not print")
+# num = int(input("Enter the num : "))
+# if(num%7 ==0):
+#     print("Yes")
+# else:
+#     print("Not")
+
+# list = [2,5,8,7]
+# list.append(4)
+# print(list)
+
+# list = [2,6,5,3,9,7]
+# list.sort(reverse = True)
+# print(list)
+# list = ['q','r','i','k','g','a']
+# list.reverse()
+# print(list)
+
+# list = [2,3,5,3]
+# list.insert(1,7)
+# print(list)
+
+# list.remove(3)
+# print(list)
+# list.pop(2)
+# print(list)
+# list.
+
+# print(type(tup))
+# print(tup[1])
+# tup[0] = 2
+# print(tup)
+# tup.slice()
+# print(tup[1:3])
+# tup = (2,5,2,2,4,7,6)
+# print(tup.index(5))
+# print(tup.count(2))
+# movies = []
+# movies.append(input("Enter the name1: "))
+# movies.append(input("Enter the name2: "))
+# movies.append(input("Enter the name3: "))
+# # list = [name1,name2,name3]
+# print(movies)
+
+# list1 = []
+# list1.append(int(input("Enter the values: ")))
+# list1.append(int(input("Enter the values: ")))
+# list1.append(int(input("Enter the values: ")))
+
+# print(list1)
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+# tup = ("C","D","A","A","B","B","A")
+
+# print(tup.count("A"))
+# list = ["C","D","A","A","B","B","A"]
+# list.sort()
+# print(list)
+
+# movies = []
+# movies.append(input("Enter movie name1: "))
+# movies.append(input("Enter movie name2: "))
+# movies.append(input("Enetr movie name3: "))
+
+# print(movies)
+
+
+# list = [1,2,3,4,3,2,1]
+
+# copy_list1 = list.copy()w
+# copy_list1.reverse()
+
+# if(list == copy_list1):
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# tup = ("C","D","A","A","B","B","A")
+
+# print(tup.count("A"))
+
+# list = ["C","D","A","A","B","B","A"]
+# list.sort()
+# print(list)
+
+# def calc_avg(a,b,c):
+#     sum = a+b+c
+#     avg = sum/3
+#     # print(avg)
+#     return avg
+# print(calc_avg(98,77,22))
+
+# print("rushikeshkasav", end="$")
+
+# def  cal_prod( a=1,b=1):
+#     print(a*b)
+#     return a*b
+
+# cal_prod()
+
+#qs-1
+# num = [7,4,5,3,21,29,13]
+# heros = ["thor","ironman","captan america","shaktiman"]
+# def calc_fun(list):
+#     print(len(list))
+
+# calc_fun(num)
+# calc_fun(heros)
+
+#qs-2
+# num = [2,3,4,2,1,4,5,6]
+# heros = ["shaktiman","man hattern","nashik","dubai"]
+
+# print(heros[0],end =" ")
+# print(heros[1], end="\n")
+
+# def print_len(list):
+#     print(len(list))
+ 
+# def print_list(list):
+#     for item in list:
+#         print(item, end=" ")
+
+# print_list(heros)
+
+#qs-3
+# num = int(input("Enter the num: "))
+
+# def calc_factorial(num):
+#     fact = 1
+#     for i in range(1 ,num+1):
+#         fact *= i
+#     print(fact)
+
+# calc_factorial(num)
+
+#qs -4 (US Dollar ≈ ₹89.6 Indian Rupees)
+
+# doller = int(input("Enter the Amount: "))
+
+# def calc_cur(doller):
+#     INR = 89.6*doller
+#     return INR
+# print(calc_cur(doller))
+
+# Qs -5
+# num = int(input("Enter the num: "))
+
+# def calc_EvenOdd(num):
+#     if(num%2 == 0):
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# calc_EvenOdd(num)
+
+###################Recursion#######################
+# qs )print reverse number
+# num = int(input("Enter the value: "))
+# def show(num):
+#     if(num == 0):
+#         return num
+#     print(num)
+#     show(num-1)
+# show(num)
+
+#qs 2) print factorial
+
+# num = int(input("Enter the num: "))
+# def calc_factorial(num):
+#     if(num == 0 or num == 1):
+#         return 1
+#     else:
+#         return num*calc_factorial(num-1)
+    
+# print(calc_factorial(num))
+
+# n = int(input("Enter the num: "))
+# i =1
+# sum = 0
+# def calc_sum(n):
+#     sum += i
+#     i -= n
+#     return sum
+# calc_sum(n)
+
+# qs - calculate sum of n natural number
+# def calc_sum(n):
+#     if(n == 0):
+#         return 0
+#     return calc_sum(n-1) + n
+# sum = calc_sum(10)
+# print(sum)
+
+# list = [1,3,2,4,2,4,3,5,7,8]
+# idx = 0
+
+# def calc_list(list,idx):
+#     if(idx == len(list)):
+#         return
+#     print(list[idx])
+#     calc_list(list,idx+1)
+
+# fruits = ["mango","lichii","apple","banan"]
+# calc_list(fruits,1)
+
+# list = [1,2,3,4,5,6,6,5,4]
+# def calc_len(list):
+#     return len(list)
+# print(calc_len(list))
+
+# list = [1,2,3,4,5,6,6,5,4]
+
+# def calc_element(list):
+#     return list
+# print(calc_element(list))
+
+# n = int(input("Enter the number: "))
+# fact =1
+# idx =0
+
+# def calc_factorial(n):
+#     fact *= idx
+
+# num = int(input("Enter the number: "))
+
+# def cal_functional(num):
+#     if(num == 0 or num == 1):
+#         return 1
+#     else:
+#         return num*cal_functional(num-1)
+# print(cal_functional(num))
+
+# USD = int(input("Eneter the usd amount: "))
+
+# def calc_usd(USD):
+#     if(USD == 0):
+#         return 0
+#     else:
+#         convert = 98.6*USD
+#     return convert
+
+# print(calc_usd(USD))
+
+
+# num = int(input("Enter the number: "))
+# def calc_sum(num):
+#     sum = 0
+#     idx = 1
+#     if(idx <= num):
+#         sum += idx
+#         idx += 1
+#         return sum
+# print(calc_sum(num))
+
+# n = int(input("Enter the value if n: "))
+# def calc_sum(n):
+#     if( n == 0):
+#         return 0
+#     return calc_sum(n-1)+n
+# sum = calc_sum(10)
+# print(sum)
+
+# list = [1,2,3,4,6,6,78,85,4,43,2,35]
+# idx = 0
+# def calc_list(list,idx):
+#     if(idx == len(list)):
+#         return
+#     print(list[idx])
+#     (calc_list(list,idx+1))
+# print(calc_list(list,4))
+
+# f = open("sample.txt","w")
+# data = f.read()
+# print(data)
+
+# data1 = f.readline()
+# print(data1)
+# # print(type(data))
+# f.close()
+# f.write(" andi mani sandiI want to learn javascript tommorow , 123 with any how condition....")
+
+# f.close()
+
+# f = open("demo.txt","w+")
+# f.write("abc")
+# f.close()
+
+# with open("demo.txt","r") as f:
+#     data = f.read()
+#     print(data)
+
+# with open("demo.txt","w") as f:
+#     f.write("new data")
+
+####################################Deleting a file
+# import os
+
+# os.remove("demo.txt")
+
+# with open("practice.txt","r") as f:
+#     # f.write("Hi everyone \nwe are learning File I/O\n")
+#     # f.write("using Java.\nI like programming in java.")
+
+#     data = f.read()
+# new_data = data.replace("Java","Django")
+# print(new_data)
+
+# with open("practice.txt","w") as f:
+#     f.write(new_data)
+
+# def chek_for_word():
+#     word = "learning"
+#     with open("practice.txt","r") as f:
+#       data = f.read()
+#     if(data.find(word) != -1):
+#         print("found")
+#     else:
+#         print("not found")  
+    
+# chek_for_word()
+
+# def check_for_line():
+#     word = "learning"
+#     data = True
+#     line_no = 1
+#     with open("practice.txt","r") as f:
+#         while data:
+#             data = f.readline()
+#             if(data.findl)
+
+# with open("practice.text","r"):
+#     data = f.read()
+#     print(data)
+
+#     num = "" 
+#     for i in range(len(data)):
+
+# class student:
+#     name = "Rushikesh kasav"
+    
+#     s1 = student()
+#     print(s1.name)
+
+class Car:
+
+    def __init__(self):
+        print("Adding new student in database..")
+    s1 = Student()
+    color = "blue"
+    brand = "mercedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
