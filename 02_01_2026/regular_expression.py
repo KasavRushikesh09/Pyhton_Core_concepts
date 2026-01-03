799c8f642e9af4082deffdbc56d04451fc068869
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-01-03 10:00:45 +0530

    regular Expression ex

---
+++ 02_01_2026/regular_expression.py
@@ -0,0 +1,55 @@
+import re
+# text = "Python is super easy"
+# regex = r"Python|super|is|easy|no"
+# l = re.findall(regex, text)
+# print(l)
+#
+# text = "a whole hole is not wwwhole"
+# regex = r"w*hole"
+# l = re.findall(regex,text)
+# print(l)
+
+# text = "i know  that no one is there in the school now"
+# regex = r"k?now"
+# l = re.findall(regex,text)
+# print(l)
+# print("Number of occurences =",len(l))
+
+# import re
+#
+# text = '''gogle
+# google
+# gooogle
+# goooogle
+# gooooogle
+# goooooogle
+# gooooooogle
+# goooooooooogle'''
+#
+# regex = r"go{2}gle"
+# l = re.findall(regex, text)
+# print(l)
+# print(len(l))
+import re
+# text = "python has nothing to do with the snake python:8t8t9r8958959"
+# regex = r"^python"
+# match = re.search(regex,text)
+# print(match)
+# regex = r"\w"
+# result = re.findall(regex, text)
+# print(result)
+# print("Number of occurences: ",len(result))
+'''    '.' meta charector   '''
+# txt = "python is super easy."
+# regex = r"."
+# match =re.findall(regex, txt)
+# print(match)
+
+'''     '/' meta charector   '''
+# txt = "python is super easy."
+# regex = r"\."
+# l = re.findall(regex, txt)
+# print(l)
+
+
+
