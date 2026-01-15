ca985972e447bab24ca7904f6981e6c32f462851
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-01-15 18:26:11 +0530

    decorator  with paramenter

---
+++ 15_01_2026/decorator_with_parameters.py
@@ -0,0 +1,12 @@
+def repeat(times):
+    def decorator(func):
+        def wrapper(*args,**kwargs):
+            for _ in range(times):
+                func(*args,**kwargs)
+        return wrapper
+    return decorator
+
+@repeat(3)
+def fun():
+    print("Hello")
+fun()
